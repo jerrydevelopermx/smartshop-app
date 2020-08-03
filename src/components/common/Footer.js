1509a2a75dc7982646636da50f74ac83b8acd762
Gerardo Garcia <gerardo.garcia@unosquare.com>
2020-08-03 02:56:40 -0500

    Added social networks icons to footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,48 +1,38 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { NavHashLink as NavLink } from "react-router-hash-link";
+import SocialMedia from "./SocialMedia";
 
 function Footer(props) {
   return (
     <footer className={props.classes.footer}>
-      <Grid container spacing={4}>
+      <Grid container spacing={2}>
         {props.content &&
           props.content.columns.length > 0 &&
           props.content.columns.map((column) => (
             <Grid item key={column.id} xs={6} sm={6} md={3}>
-              <div style={{ margin: "auto", textAlign: "center" }}>
+              <div className={props.classes.centeredContent}>
                 {column.title}
-                <div
-                  style={{
-                    width: "100%",
-                    margin: "auto",
-                    textAlign: "left",
-                    border: "1px solid",
-                  }}
-                >
-                  <ul style={{ listStyleType: "none", padding: "5% 15%" }}>
+                <div className={props.classes.footerUlList}>
+                  <ul style={{ listStyleType: "circle", padding: "5% 15%" }}>
                     {column.options.map((option, index) => (
                       <li key={"footOpt" + index}>
-                        <NavLink
-                          style={{
-                            color: "#fff",
-                            textDecoration: "none",
-                          }}
-                          to=""
-                        >
+                        <NavLink className={props.classes.footerLinks} to="">
                           {option.text}
                         </NavLink>
                       </li>
                     ))}
                   </ul>
+                  <div className={props.classes.centeredContent}>
+                    {column.social ? (
+                      <SocialMedia
+                        networks={column.social}
+                        classes={props.classes}
+                      />
+                    ) : null}
+                  </div>
                 </div>
               </div>
-              <div
-                style={{
-                  margin: "auto",
-                  textAlign: "left",
-                }}
-              ></div>
             </Grid>
           ))}
       </Grid>
---
+++ src/components/common/SocialMedia.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function SocialMedia(props) {
+  return (
+    <div>
+      {props.networks.length > 0 &&
+        props.networks.map((network, index) => (
+          <img
+            key={"netw-" + index}
+            src={`${process.env.PUBLIC_URL}/imgs/icons/${network}-icon.png`}
+            alt={network}
+            className={props.classes.socialMediaIcons}
+          />
+        ))}
+    </div>
+  );
+}
+
+export default SocialMedia;
